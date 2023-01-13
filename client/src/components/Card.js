1fde28b89057fdf7459e8da7c36577c4c9f9e227
forestleigh <theforestleigh@gmail.com>
2023-01-13 13:35:13 -0800

    simplified and organized components

---
+++ client/src/components/Card.js
@@ -0,0 +1,20 @@
+import React from "react";
+import me from "../../src/assets/me.png";
+import "../../src/styles/card.scss"
+
+const Card = () => (
+  <div class="card">
+    <img src={me} alt="Forest" width="130px" />
+    <h1>Forest Leigh</h1>
+    <p class="title">Software Engineer</p>
+    <p class="title">Caltech</p>
+    <a href="https://github.com/forestleigh" target="_blank">
+      <i class="fa fa-github"></i>
+    </a>
+    <a href="https://www.linkedin.com/in/forestleigh" target="_blank">
+      <i class="fa fa-linkedin"></i>
+    </a>
+  </div>
+);
+
+export default Card;

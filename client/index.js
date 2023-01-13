81981bf5621e7a2b343d6e0a5fbc9417af8332f3
forestleigh <theforestleigh@gmail.com>
2023-01-13 12:58:50 -0800

    selected background photo from options

--- client/index.js
+++ client/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import { createRoot, render } from "react-dom/client";
 import App from "./src/App";
 import "./src/styles/index.css";
-import "./src/assets/forest.png"
 
 const container = document.getElementById("root");
 const root = createRoot(container);

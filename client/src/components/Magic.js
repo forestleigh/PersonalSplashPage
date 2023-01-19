ecb2288c987d174cfb185d5c9a30c149adc69936
forestleigh <theforestleigh@gmail.com>
2023-01-18 16:09:17 -0800

    found workaround for scale transform and have animation complete

--- client/src/components/Magic.js
+++ client/src/components/Magic.js
@@ -1,28 +1,16 @@
 import React from "react";
 import "../../src/styles/magic.scss";
-import { useMousePosition } from "../utils";
 
 const Magic = () => {
-  const mousePosition = useMousePosition();
-
-  // pass updated positions to css for perforance animation
-  // document.documentElement.style.setProperty('--x', mousePosition.x);
-  // document.documentElement.style.setProperty('--y', mousePosition.y);
-  // console.log("mouse", mousePosition.x, mousePosition.y);
 
   const Particles = [];
 
-    for (let i = 0; i < 250; i++) {
+    for (let i = 0; i < 100; i++) {
       Particles.push(<div className="particle" key={i}></div>);
     }
 
   return (
     <>
-    {/* <p>
-      Your cursor position:
-      <br />
-      {JSON.stringify(mousePosition)}
-    </p> */}
     <div id="particle-container">{ Particles }</div>
     </>
   );

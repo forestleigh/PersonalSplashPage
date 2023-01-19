2afbd1b13d08f84d714829674c8b0cb6fb4e116d
forestleigh <theforestleigh@gmail.com>
2023-01-19 13:47:11 -0800

    updated port for aws config

--- dockerfile
+++ dockerfile
@@ -3,5 +3,5 @@ FROM node:16.16
 WORKDIR /usr/src/app
 COPY . .
 RUN npm install && npm run build
-EXPOSE 3000
+EXPOSE 8081
 ENTRYPOINT ["node", "./server/server.js"]
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const cors = require("cors");
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8081;
 
 // handle parsing request body
 app.use(cors());
